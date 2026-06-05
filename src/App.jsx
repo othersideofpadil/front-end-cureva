b5444d6b1c6f0786174da1ebb79764fc28363438
padilkecebanget <114470786+padilkecebanget@users.noreply.github.com>
2026-06-05 22:12:00 +0700

    remove: file ga kepake

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import LandingPage from "./pages/LandingPage";
 const Login = lazy(() => import("./pages/auth/Login"));
 const Register = lazy(() => import("./pages/auth/Register"));
 const ForgotPassword = lazy(() => import("./pages/auth/ForgotPassword"));
-const Dashboard = lazy(() => import("./pages/dashboard/Dashboard"));
 const BookingList = lazy(() => import("./pages/booking/BookingList"));
 const BookingDetail = lazy(() => import("./pages/booking/BookingDetail"));
 const CreateBooking = lazy(() => import("./pages/booking/CreateBooking"));
@@ -59,20 +58,6 @@ function App() {
           <Page>
             <ForgotPassword />
           </Page>
-        }
-      />
-
-      {/* Protected Routes */}
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <MainLayout>
-              <Page>
-                <Dashboard />
-              </Page>
-            </MainLayout>
-          </ProtectedRoute>
         }
       />
 
